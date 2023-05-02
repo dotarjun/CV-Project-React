42393c1b43b52ba9337a182a24081cf67b78e02d
Arjun <arjunsingh8112@gmail.com>
2023-05-02 15:24:10 +0530

    🎉 feat(main.js): add components for user info, contact, experience, projects, and skills

--- src/Components/main.js
+++ src/Components/main.js
@@ -1,9 +1,18 @@
 import React from 'react'
+import UserInfo from "./Main Components/userInfo";
+import Contact from "./Main Components/contact";
+import Experience from "./Main Components/experience";
+import Projects from "./Main Components/projects";
+import Skills from "./Main Components/skills";
 
 export default function Main() {
     return (
         <div>
-
+            <UserInfo />
+            <Contact />
+            <Experience />
+            <Projects />
+            <Skills />
         </div>
     )
 }
