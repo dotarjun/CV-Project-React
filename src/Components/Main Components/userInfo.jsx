82dd91b719d4a2b6bfc95028e4e95bd5c55d26ae
Arjun <arjunsingh8112@gmail.com>
2023-05-03 18:42:17 +0530

    🎨 style(userInfo.jsx): improve styling of Personal Information section

--- src/Components/Main Components/userInfo.jsx
+++ src/Components/Main Components/userInfo.jsx
@@ -2,12 +2,13 @@ import React from 'react';
 
 function UserInfo() {
   return (
-    <div>
-      <h3>Personal Information</h3>
-
-      <input name="name" type="text" placeholder="Name" />
-      <input name="position" type="text" placeholder="Position" />
-      <textarea name="description" placeholder="Description" />
+    <div className="mx-8">
+      <h3 className="text-3xl my-3">Personal Information</h3>
+      <div className="flex flex-col gap-6 my-6">
+        <input className="bg-slate-400 opacity-80 outline-none rounded text-slate-950 p-2 placeholder:text-slate-700" name="name" type="text" placeholder="Name" />
+        <input className="bg-slate-400 opacity-80 outline-none rounded text-slate-950 p-2 placeholder:text-slate-700" name="position" type="text" placeholder="Position" />
+        <textarea className="bg-slate-400 opacity-80 outline-none rounded text-slate-950 p-2 placeholder:text-slate-700" name="description" placeholder="Description" />
+      </div>
     </div>
   );
 }
