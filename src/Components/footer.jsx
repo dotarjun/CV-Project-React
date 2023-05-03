41005f37074d3ee981ee1611e1d54b7f6659a95c
Arjun <arjunsingh8112@gmail.com>
2023-05-03 18:18:02 +0530

    🎨 style(footer.jsx): add styling to footer component

--- src/Components/footer.jsx
+++ src/Components/footer.jsx
@@ -2,11 +2,11 @@ import React from 'react';
 
 export default function Footer() {
   return (
-    <footer>
-      <p>
+    <footer className="bg-slate-900 absolute w-screen bottom-0">
+      <p className="text-3xl text-center py-8 text-slate-400">
         Copyright © 2023
         {' '}
-        <a href="https://github.com/dotarjun">Arjun Singh</a>
+        <a href="https://github.com/dotarjun" className="text-slate-200">Arjun Singh</a>
       </p>
     </footer>
   );
