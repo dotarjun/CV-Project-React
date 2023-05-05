003d14389b6a1a26d5352c1c2cc1e768b5814de7
Arjun <arjunsingh8112@gmail.com>
2023-05-05 10:04:43 +0530

    🔧 chore(tailwind.config.js): add daisyui and typography plugins and configure daisyui theme options

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,5 @@
+/* eslint-disable import/no-extraneous-dependencies */
+/* eslint-disable global-require */
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
@@ -6,5 +8,11 @@ module.exports = {
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [
+    require('@tailwindcss/typography'),
+    require('daisyui'),
+  ],
+  daisyui: {
+    themes: ['forest', 'corporate'],
+  },
 };
