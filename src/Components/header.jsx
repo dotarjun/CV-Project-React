8296f24097c62bd4a9d299c7fa24f8654c351f79
Arjun <arjunsingh8112@gmail.com>
2023-05-05 10:04:58 +0530

    💄 style(header.jsx): update header styles to use new design system classes

--- src/Components/header.jsx
+++ src/Components/header.jsx
@@ -2,7 +2,8 @@ import React from 'react';
 
 export default function Header() {
   return (
-    <header className="bg-slate-900 py-8 text-5xl text-center font-bold">
+    <header className=" bg-neutral-focus w-screen py-10 text-5xl text-center text-neutral-content  font-bold">
+      {/* bg-slate-900 py-8 text-5xl text-center font-bold */}
       <h1>CV Creator</h1>
     </header>
   );
