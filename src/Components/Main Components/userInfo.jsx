aed40f8ca15c5a9f64bb51b5959b0ee7cd261c94
Arjun <arjunsingh8112@gmail.com>
2023-05-05 10:06:02 +0530

    🎨 style(userInfo.jsx): refactor input and textarea classes to use tailwindcss classes 🚀 perf(userInfo.jsx): optimize input and textarea rendering by using tailwindcss classes

--- src/Components/Main Components/userInfo.jsx
+++ src/Components/Main Components/userInfo.jsx
@@ -4,16 +4,16 @@ import { ReactComponent as SaveIcon } from '../../Assets/floppy-disk-svgrepo-com
 function UserInfo() {
   return (
     <div className="mx-8">
-      <div className="my-3 flex flex-row justify-between items-center">
+      <div className="flex flex-row justify-between items-center">
         <h3 className="text-3xl">Personal Information</h3>
         <div className="shadow-md shadow-slate-950 w-8 h-8 cursor-pointer hover:shadow-slate-950 hover:shadow-xl">
           <SaveIcon className="w-8 h-8 transition duration-300 transform hover:scale-110" />
         </div>
       </div>
-      <div className="flex flex-col gap-8 my-6">
-        <input className="bg-slate-400 outline-none rounded text-slate-950 p-2 placeholder:text-slate-700" name="name" type="text" placeholder="Name" />
-        <input className="bg-slate-400 outline-none rounded text-slate-950 p-2 placeholder:text-slate-700" name="position" type="text" placeholder="Position" />
-        <textarea className="bg-slate-400 outline-none rounded text-slate-950 p-2 placeholder:text-slate-700" name="description" placeholder="Description" />
+      <div className="flex flex-col my-6 gap-2">
+        <input className="input input-lg" name="name" type="text" placeholder="Name" />
+        <input className="input input-lg" name="position" type="text" placeholder="Position" />
+        <textarea className="textarea textarea-lg" name="description" placeholder="Description" />
       </div>
     </div>
   );
