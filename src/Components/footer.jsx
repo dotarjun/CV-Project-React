dbec234fd1d6925b30e98278ca0ec8040d172190
Arjun <arjunsingh8112@gmail.com>
2023-05-05 10:05:17 +0530

    🎨 style(footer.jsx): change background color to neutral-focus and text color to base-content 🔥 chore(footer.jsx): remove unused CSS classes 🚀 feat(footer.jsx): add hover effect to the link in the footer

--- src/Components/footer.jsx
+++ src/Components/footer.jsx
@@ -2,11 +2,11 @@ import React from 'react';
 
 export default function Footer() {
   return (
-    <footer className="bg-slate-900 absolute w-screen bottom-0">
-      <p className="text-3xl text-center py-8 text-slate-400">
+    <footer className="bg-neutral-focus absolute w-screen bottom-0">
+      <p className="text-3xl text-center py-10 text-base-content">
         Copyright © 2023
         {' '}
-        <a href="https://github.com/dotarjun" className="text-slate-200">Arjun Singh</a>
+        <a href="https://github.com/dotarjun" className="text-accent hover:text-accent-focus">Arjun Singh</a>
       </p>
     </footer>
   );
