d51e72e6d9535bcc6e8a66e272f638a0a2a8f5c4
Arjun <arjunsingh8112@gmail.com>
2023-05-07 00:07:15 +0530

    🔥 refactor(main.jsx): remove Projects and Skills components from Main component ✨ feat(main.jsx): add form element to wrap UserInfo, Experience, Contact, and Save components ✨ feat(main.jsx): add submit button to form element

--- src/Components/main.jsx
+++ src/Components/main.jsx
@@ -2,19 +2,22 @@ import React from 'react';
 import UserInfo from './Main Components/userInfo';
 import Contact from './Main Components/contact';
 import Experience from './Main Components/experience';
-import Projects from './Main Components/projects';
-import Skills from './Main Components/skills';
+// import Projects from './Main Components/projects';
+// import Skills from './Main Components/skills';
 import Save from './save';
 
 export default function Main() {
   return (
     <main className="bg-neutral min-w-full min-h-full prose">
-      <UserInfo />
-      <Contact />
-      <Experience />
-      <Projects />
-      <Skills />
-      <Save />
+      <form className="mx-8">
+        <UserInfo />
+        <Experience />
+        <Contact />
+        {/* <Projects /> */}
+        {/* <Skills /> */}
+        <Save />
+        <button type="submit" className="btn btn-accent btn-block my-4">Submit</button>
+      </form>
     </main>
   );
 }
