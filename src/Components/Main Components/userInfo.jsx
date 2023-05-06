71083c88141765c1e9baccdd769de0f3160f0a30
Arjun <arjunsingh8112@gmail.com>
2023-05-06 15:13:29 +0530

    🔥 chore(userInfo.jsx): comment out SaveIcon import and usage

--- src/Components/Main Components/userInfo.jsx
+++ src/Components/Main Components/userInfo.jsx
@@ -1,13 +1,13 @@
 import React from 'react';
-import { ReactComponent as SaveIcon } from '../../Assets/floppy-disk-svgrepo-com.svg';
+// import { ReactComponent as SaveIcon } from '../../Assets/floppy-disk-svgrepo-com.svg';
 
 function UserInfo() {
   return (
     <div className="mx-8">
       <div className="flex flex-row justify-between items-center">
-        <h3 className="text-3xl">Personal Information</h3>
+        <h3 className="">Personal Information</h3>
         <div className="shadow-md shadow-slate-950 w-8 h-8 cursor-pointer hover:shadow-slate-950 hover:shadow-xl">
-          <SaveIcon className="w-8 h-8 transition duration-300 transform hover:scale-110" />
+          {/* <SaveIcon className="w-8 h-8 transition duration-300 transform hover:scale-110" /> */}
         </div>
       </div>
       <div className="flex flex-col my-6 gap-2">
