f1be089de2779dea6503d6f06e44363b33dbc723
Arjun <arjunsingh8112@gmail.com>
2023-05-07 00:05:45 +0530

    ✨ feat(contact.jsx): add contact form with email, GitHub and LinkedIn inputs

--- src/Components/Main Components/contact.jsx
+++ src/Components/Main Components/contact.jsx
@@ -1,8 +1,19 @@
 import React from 'react';
+import Input from './input';
 
 function Contact() {
   return (
-    <div />
+    <div>
+      <div className="flex flex-row justify-between items-center">
+        <h3 className="">Contact</h3>
+        <div className="shadow-md shadow-slate-950 w-8 h-8 cursor-pointer hover:shadow-slate-950 hover:shadow-xl" />
+      </div>
+      <div className="flex flex-col my-6 gap-2">
+        <Input placeholder="Email" type="mail" />
+        <Input placeholder="GitHub" />
+        <Input placeholder="LinkedIn" />
+      </div>
+    </div>
   );
 }
 
