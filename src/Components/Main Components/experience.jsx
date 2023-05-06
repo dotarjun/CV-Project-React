9db5dd1644b9329a9dda7ee173fdec82c23eb3ab
Arjun <arjunsingh8112@gmail.com>
2023-05-07 00:06:40 +0530

    ✨ feat(experience.jsx): add button to add work experience 🎨 style(experience.jsx): add styling to experience section header and add button

--- src/Components/Main Components/experience.jsx
+++ src/Components/Main Components/experience.jsx
@@ -1,9 +1,16 @@
 import React from 'react';
+import WorkExp from './workExp';
 
 function Experience() {
   return (
     <div>
-      <h3>Experience</h3>
+      <div className="flex flex-row justify-between items-center">
+        <h3 className="">Experience</h3>
+        <div className="shadow-md shadow-slate-950 w-8 h-8 cursor-pointer hover:shadow-slate-950 hover:shadow-xl" />
+      </div>
+      <WorkExp />
+
+      <button type="button" className="btn btn-block bg-base-100 hover:border-base-content hover:border-2 hover:bg-base-100 hover:border-opacity-30  text-2xl">+</button>
     </div>
   );
 }
