4b57a8bc37455568e538e8118077847652337b09
Arjun <arjunsingh8112@gmail.com>
2023-05-06 15:12:58 +0530

    🎨 style(footer.jsx): remove text-3xl class from footer text to improve UI consistency

--- src/Components/footer.jsx
+++ src/Components/footer.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 export default function Footer() {
   return (
     <footer className="bg-neutral-focus absolute w-screen bottom-0">
-      <p className="text-3xl text-center py-10 text-base-content">
+      <p className="text-center py-10 text-base-content">
         Copyright © 2023
         {' '}
         <a href="https://github.com/dotarjun" className="text-accent hover:text-accent-focus">Arjun Singh</a>
