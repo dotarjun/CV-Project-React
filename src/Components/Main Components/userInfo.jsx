4f84ae1c7d0e74402328f1cb59a1e0ce1b52b552
Arjun <arjunsingh8112@gmail.com>
2023-05-07 00:05:34 +0530

    🎨 style(userInfo.jsx): replace input and textarea with custom Input component

--- src/Components/Main Components/userInfo.jsx
+++ src/Components/Main Components/userInfo.jsx
@@ -1,19 +1,18 @@
 import React from 'react';
+import Input from './input';
 // import { ReactComponent as SaveIcon } from '../../Assets/floppy-disk-svgrepo-com.svg';
 
 function UserInfo() {
   return (
-    <div className="mx-8">
+    <div>
       <div className="flex flex-row justify-between items-center">
         <h3 className="">Personal Information</h3>
-        <div className="shadow-md shadow-slate-950 w-8 h-8 cursor-pointer hover:shadow-slate-950 hover:shadow-xl">
-          {/* <SaveIcon className="w-8 h-8 transition duration-300 transform hover:scale-110" /> */}
-        </div>
+        <div className="shadow-md shadow-slate-950 w-8 h-8 cursor-pointer hover:shadow-slate-950 hover:shadow-xl" />
       </div>
       <div className="flex flex-col my-6 gap-2">
-        <input className="input input-lg" name="name" type="text" placeholder="Name" />
-        <input className="input input-lg" name="position" type="text" placeholder="Position" />
-        <textarea className="textarea textarea-lg" name="description" placeholder="Description" />
+        <Input placeholder="Name" />
+        <Input placeholder="Position" />
+        <textarea className="textarea textarea-lg my-2" name="description" placeholder="Description" />
       </div>
     </div>
   );
