3c9c5303abaa197c5377e26dfa32fd64f212545a
Arjun <arjunsingh8112@gmail.com>
2023-05-09 12:39:35 +0530

    🔥 chore(userInfo.jsx): remove unused Input component import ✨ feat(userInfo.jsx): replace Input component with native input element and add classNames to inputs

--- src/Components/Main Components/userInfo.jsx
+++ src/Components/Main Components/userInfo.jsx
@@ -1,6 +1,4 @@
 import React from 'react';
-import Input from './input';
-// import { ReactComponent as SaveIcon } from '../../Assets/floppy-disk-svgrepo-com.svg';
 
 function UserInfo() {
   return (
@@ -10,8 +8,14 @@ function UserInfo() {
         <div className="shadow-md shadow-slate-950 w-8 h-8 cursor-pointer hover:shadow-slate-950 hover:shadow-xl" />
       </div>
       <div className="flex flex-col my-6 gap-2">
-        <Input placeholder="Name" />
-        <Input placeholder="Position" />
+        <input
+          placeholder="Name"
+          className="input input-lg"
+        />
+        <input
+          placeholder="Position"
+          className="input input-lg"
+        />
         <textarea className="textarea textarea-lg my-2" name="description" placeholder="Description" />
       </div>
     </div>
