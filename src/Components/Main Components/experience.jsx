398545afaae3ac14621d1f6b712610e32cffdb65
Arjun <arjunsingh8112@gmail.com>
2023-05-10 18:11:36 +0530

    🔥 chore(experience.jsx): remove unused div element

--- src/Components/Main Components/experience.jsx
+++ src/Components/Main Components/experience.jsx
@@ -32,7 +32,6 @@ function Experience() {
     <div>
       <div className="flex flex-row justify-between items-center">
         <h3 className="">Experience</h3>
-        <div className="shadow-md shadow-slate-950 w-8 h-8 cursor-pointer hover:shadow-slate-950 hover:shadow-xl" />
       </div>
 
       <div className="flex flex-col my-6 gap-2">
