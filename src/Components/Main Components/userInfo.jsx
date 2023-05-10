94bf1c56e96d40611250a93b210d4a238d7a381d
Arjun <arjunsingh8112@gmail.com>
2023-05-10 18:11:48 +0530

    🚀 feat(userInfo.jsx): add state hooks for Name, Position, and Bio 🐛 fix(userInfo.jsx): add onSubmit event listener to Name, Position, and Bio inputs

--- src/Components/Main Components/userInfo.jsx
+++ src/Components/Main Components/userInfo.jsx
@@ -1,22 +1,35 @@
-import React from 'react';
+/* eslint-disable no-unused-vars */
+import React, { useState } from 'react';
+import { ReactComponent as SaveIcon } from '../../Assets/save.svg';
 
 function UserInfo() {
+  const [Name, setName] = useState('');
+  const [Position, setPosition] = useState('');
+  const [Bio, setBio] = useState('');
+
   return (
     <div>
       <div className="flex flex-row justify-between items-center">
         <h3 className="">Personal Information</h3>
-        <div className="shadow-md shadow-slate-950 w-8 h-8 cursor-pointer hover:shadow-slate-950 hover:shadow-xl" />
+        <SaveIcon className=" w-9 h-9 fill-accent shadow-inner shadow-slate-700 cursor-pointer" />
       </div>
       <div className="flex flex-col my-6 gap-2">
         <input
           placeholder="Name"
           className="input input-lg"
+          onSubmit={(e) => (setName(e.target.value))}
         />
         <input
           placeholder="Position"
           className="input input-lg"
+          onSubmit={(e) => (setPosition(e.target.value))}
         />
-        <textarea className="textarea textarea-lg my-2" name="description" placeholder="Description" />
+        <textarea
+          className="textarea textarea-lg my-2"
+          name="bio"
+          placeholder="Bio"
+          onSubmit={(e) => (setBio(e.target.value))}
+        />
       </div>
     </div>
   );
