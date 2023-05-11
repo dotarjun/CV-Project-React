445fab257e0a5f614d89ffbbc0767d143d8f645d
Arjun <arjunsingh8112@gmail.com>
2023-05-11 20:01:09 +0530

    🎨 style(userInfo.jsx): add form and submit button to save user information 🐛 fix(userInfo.jsx): fix input fields not updating state on change

--- src/Components/Main Components/userInfo.jsx
+++ src/Components/Main Components/userInfo.jsx
@@ -1,36 +1,57 @@
 /* eslint-disable no-unused-vars */
 import React, { useState } from 'react';
-import { ReactComponent as SaveIcon } from '../../Assets/save.svg';
 
 function UserInfo() {
   const [Name, setName] = useState('');
   const [Position, setPosition] = useState('');
   const [Bio, setBio] = useState('');
+  const [UInfo, setUInfo] = useState([]);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    setUInfo({
+      Name,
+      Position,
+      Bio,
+    });
+  };
+  console.log(UInfo);
 
   return (
     <div>
       <div className="flex flex-row justify-between items-center">
         <h3 className="">Personal Information</h3>
-        <SaveIcon className=" w-9 h-9 fill-accent shadow-inner shadow-slate-700 cursor-pointer" />
+        {/* <SaveIcon className=" w-9 h-9 fill-accent shadow-
+        inner shadow-slate-700 cursor-pointer" /> */}
       </div>
-      <div className="flex flex-col my-6 gap-2">
+      <form className="flex flex-col my-6 gap-2" onSubmit={handleSubmit}>
         <input
           placeholder="Name"
           className="input input-lg"
-          onSubmit={(e) => (setName(e.target.value))}
+          value={Name}
+          onChange={(e) => (setName(e.target.value))}
         />
         <input
           placeholder="Position"
           className="input input-lg"
-          onSubmit={(e) => (setPosition(e.target.value))}
+          value={Position}
+          onChange={(e) => (setPosition(e.target.value))}
         />
         <textarea
           className="textarea textarea-lg my-2"
           name="bio"
           placeholder="Bio"
-          onSubmit={(e) => (setBio(e.target.value))}
+          value={Bio}
+          onChange={(e) => (setBio(e.target.value))}
         />
-      </div>
+        <button
+          type="submit"
+          className="btn btn-block bg-base-100 hover:border-base-content hover:border-2 hover:bg-base-100 hover:border-opacity-30  text-sm text-"
+        >
+          Save
+        </button>
+      </form>
     </div>
   );
 }
