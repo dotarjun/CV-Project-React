2651c6f202b1fdf965ffbc6a5fb3828424b6f38f
Arjun <arjunsingh8112@gmail.com>
2023-05-13 16:45:19 +0530

    🎨 style(contact.jsx): add form submission confirmation message

--- src/Components/Main Components/contact.jsx
+++ src/Components/Main Components/contact.jsx
@@ -6,14 +6,21 @@ function Contact() {
   const [Github, setGithub] = useState('');
   const [LinkedIn, setLinkedIn] = useState('');
   const [UserContactInfo, setUserContactInfo] = useState([]);
+  const [submitted, setSubmitted] = useState(false);
 
   const handleSubmit = (e) => {
     e.preventDefault();
+
     setUserContactInfo({
       Email,
       Github,
       LinkedIn,
     });
+
+    setEmail('');
+    setGithub('');
+    setLinkedIn('');
+    setSubmitted(true);
   };
 
   return (
@@ -22,30 +29,52 @@ function Contact() {
         <h3 className="">Contact</h3>
         <div className="shadow-md shadow-slate-950 w-8 h-8 cursor-pointer hover:shadow-slate-950 hover:shadow-xl" />
       </div>
-      <form className="flex flex-col my-6 gap-2" onSubmit={handleSubmit}>
-        <input
-          type="text"
-          placeholder="Email"
-          className="input input-lg"
-          value={Email}
-          onChange={(e) => (setEmail(e.target.value))}
-        />
-        <input
-          type="text"
-          placeholder="GitHub"
-          className="input input-lg"
-          value={Github}
-          onChange={(e) => (setGithub(e.target.value))}
-        />
-        <input
-          type="text"
-          placeholder="LinkedIn"
-          className="input input-lg"
-          value={LinkedIn}
-          onChange={(e) => (setLinkedIn(e.target.value))}
-        />
-        <Save />
-      </form>
+      {!submitted ? (
+        <form className="flex flex-col my-6 gap-2" onSubmit={handleSubmit}>
+          <input
+            type="text"
+            placeholder="Email"
+            className="input input-lg"
+            value={Email}
+            onChange={(e) => (setEmail(e.target.value))}
+          />
+          <input
+            type="text"
+            placeholder="GitHub"
+            className="input input-lg"
+            value={Github}
+            onChange={(e) => (setGithub(e.target.value))}
+          />
+          <input
+            type="text"
+            placeholder="LinkedIn"
+            className="input input-lg"
+            value={LinkedIn}
+            onChange={(e) => (setLinkedIn(e.target.value))}
+          />
+          <Save />
+        </form>
+      ) : (
+        <div>
+          {submitted && (
+            <div>
+              <p>
+                Email:
+                {` ${UserContactInfo.Email}`}
+              </p>
+              <p>
+                GitHub:
+                {` ${UserContactInfo.Github}`}
+              </p>
+              <p>
+                LinkedIn:
+                {` ${UserContactInfo.LinkedIn}`}
+              </p>
+            </div>
+          )}
+        </div>
+      )}
+
     </div>
   );
 }
