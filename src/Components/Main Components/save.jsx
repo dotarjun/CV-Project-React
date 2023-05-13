1a2c35d13baf84e452cede18bb8deeb0dd9e6674
Arjun <arjunsingh8112@gmail.com>
2023-05-13 16:36:59 +0530

    ✨ feat(save.jsx): add Save component to render a button to save data

---
+++ src/Components/Main Components/save.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function Save() {
+  return (
+    <button
+      type="submit"
+      className="btn btn-block bg-base-100 hover:border-base-content hover:border-2 hover:bg-base-100 hover:border-opacity-30  text-sm "
+    >
+      Save
+    </button>
+  );
+}
+
+export default Save;
