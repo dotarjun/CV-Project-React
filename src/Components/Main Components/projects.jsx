70b31f6d713af3533d28fd132f1ac9b792b3b1e8
Arjun <arjunsingh8112@gmail.com>
2023-05-14 07:40:31 +0530

    🔥 chore(projects.jsx, skills.jsx): remove unused components

--- src/Components/Main Components/projects.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-function Projects() {
-  return (
-    <div />
-  );
-}
-
-export default Projects;
--- src/Components/Main Components/skills.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-function Skills() {
-  return (
-    <div />
-  );
-}
-
-export default Skills;
